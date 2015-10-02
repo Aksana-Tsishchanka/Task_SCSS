2875007f6bd5ba99cfc2aee7bf6896a911d256b2
Aksana-Tsishchanka <oks1-92@mail.ru>
2015-10-02 16:12:12 +0300

    Fixed nav menu when scroll done

--- src/js/fixed-nav.js
+++ src/js/fixed-nav.js
@@ -1,20 +1,22 @@
 /**
  * Created by Aksana_Tsishchanka on 9/29/2015.
  */
-    var navElem =  document.getElementById('about-nav');
-    var navElemSourceBottom = navElem.getBoundingClientRect().bottom + window.pageYOffset;
+//var nav =  document.querySelector(".nav");
+//var navOuter = document.querySelector(".about-nav");
+//var nav = document.querySelector('.nav');
 
-    console.log('navElemSourceBottom: ' + navElemSourceBottom);
+var navOuter = document.querySelector('#about');
+var nav =  document.querySelector(".about-nav");
 
-    window.onscroll = function() {
-        console.log('window.pageYOffset:' + window.pageYOffset);
-        if (navElem.classList.contains('fixed-nav') && window.pageYOffset < navElemSourceBottom) {
-            navElem.classList.remove('fixed-nav');
-        } else if (window.pageYOffset > navElemSourceBottom) {
-            navElem.classList.add('fixed-nav');
-        }
+addEventListener('scroll', function() {
+    var bodyHeightScroll = document.body.scrollTop;
+    console.log('windowScroll', bodyHeightScroll);
+
+    if (nav.classList.contains('fixed-nav') && bodyHeightScroll < navOuter.offsetTop) {
+        nav.classList.remove('fixed-nav');
     }
-/*window.onscroll = function() {
- var scrolled = window.pageYOffset || document.documentElement.scrollTop;
- document.getElementById('fixed-nav').innerHTML = scrolled + 'px';
- }*/
+    else
+        if (!nav.classList.contains('fixed-nav') && bodyHeightScroll > navOuter.offsetTop) {
+            nav.classList.add('fixed-nav');
+        }
+});
